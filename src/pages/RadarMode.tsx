fcd0e5dc308a5795caaacc591d1dc74a4ce07406
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-12 17:24:53 +0100

    fix: import font family

--- src/pages/RadarMode.tsx
+++ src/pages/RadarMode.tsx
@@ -1,29 +1,33 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 export default function RadarMode() {
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="flex flex-col w-4/5 h-3/5 items-center justify-center bg-white rounded-2xl">
           <div className="flex flex-col w-4/5">
             <div>
-            <p className="text-4xl font-bold text-center font-space text-xl">Le Mode Radar</p>
+            <p className="text-xl font-bold text-center font-space">Le Mode Radar</p>
             <div>
-              <p className="text-center text-sm">Ne rate aucun drop même avec l’application fermée</p>
+              <p className="text-center text-xs leading-3">Ne rate aucun drop même avec l’application fermée</p>
             </div>
-            <div className="icon">icon</div>
+            <div className="flex items-center justify-center">
+							<Image src="/assets/radar.png" width={100} height={100} alt="Radar" />
+						</div>
           </div>
           <div className="flex flex-col items-center justify-center w-full h-full">
             <div className="flex flex-col items-center justify-center w-full h-1/2 text-justify">
               <p className="text-sm mb-2 mt-5 text-center">Comment ça marche ?</p>
-              <p className="text-xs leading-3">Nous utilisons une fonctionnalité de ton téléphone qui permet de suivre ta position en arrière plan, cela nous permet de t’envoyer une notification lorsque nous detectons que tu passe à côté d’un drop !</p>
+              <p className="text-xs leading-3 text-gray-500">Nous utilisons une fonctionnalité de ton téléphone qui permet de suivre ta position en arrière plan, cela nous permet de t’envoyer une notification lorsque nous detectons que tu passe à côté d’un drop !</p>
             </div>  
             <div className="flex flex-col items-center justify-center w-full h-1/2">
-              <p className="text-gray-600 body-font font-space-300 text-sm mb-2 mt-5 text-center">Est-ce que je vais perdre de la batterie ?</p>
-              <p className="text-gray-600 body-font font-space-300 text-xs text-justify leading-3">L’application detecte automatiquement quand tu te deplace, le reste du temps elle reste inactive pour preserver un maximum la batterie. Tu ne devrais pas remarque de changements après avoir activé le mode radar.</p>
+              <p className="text-darkGray body-font font-space-300 text-sm mb-2 mt-5 text-center">Est-ce que je vais perdre de la batterie ?</p>
+              <p className="text-gray-500 text-xs text-justify leading-3">L’application detecte automatiquement quand tu te deplace, le reste du temps elle reste inactive pour preserver un maximum la batterie. Tu ne devrais pas remarque de changements après avoir activé le mode radar.</p>
             </div>  
             <div className="flex flex-col items-center justify-center w-full h-1/2">
               <p className="text-sm mb-2 mt-5 text-center">Est-ce que vous me suivez à la trace ?</p>
-              <p className="text-xs text-justify leading-3">Nous utilisons ta position uniquement pour calculer ta distance avec les drops autour de toi, une fois trouvés, ta position n’est pas sauvegardée. Il est donc impossible de te tracer avec Dropy !</p>
+              <p className="text-xs text-gray-500 text-justify leading-3 font-space">Nous utilisons ta position uniquement pour calculer ta distance avec les drops autour de toi, une fois trouvés, ta position n’est pas sauvegardée. Il est donc impossible de te tracer avec Dropy !</p>
             </div>  
           </div>
         </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,10 @@ module.exports = {
       fontFamily: {
         'space': ['Space Grotesk', 'sans-serif']
       },
+      colors: {
+        'grey': '#7D86A9',
+        'darkGrey': '#59607A',
+      },
     },
   },
   plugins: [],
