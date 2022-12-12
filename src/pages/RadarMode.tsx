668d4efaa6a2a8b27771db0985c0ce954a1e1967
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-12 21:14:52 +0100

    feat: added BackgroundMap commponent

--- src/pages/RadarMode.tsx
+++ src/pages/RadarMode.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import Image from 'next/image';
-import map from '../../public/img/map.svg';
 import RadarBox from '../components/RadarBox';
+import BackgroundMap from '../components/BackgroundMap';
 
 export default function RadarMode() {
 
   return (
     <>
-    <div className="z-0 fixed w-screen h-screen">
-      <Image src={map} alt="background" layout="fill" objectFit='cover' />
-    </div>
+    <BackgroundMap />
     <RadarBox />
     </>
     
