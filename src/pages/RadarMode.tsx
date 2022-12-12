fc7c83ac5b3a3d7e03b81ccbe79657ad5a17e8bd
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-12 17:30:00 +0100

    feat: font family -> Space Grotesk

--- src/pages/RadarMode.tsx
+++ src/pages/RadarMode.tsx
@@ -10,7 +10,7 @@ export default function RadarMode() {
             <div>
             <p className="text-xl font-bold text-center font-space">Le Mode Radar</p>
             <div>
-              <p className="text-center text-xs leading-3">Ne rate aucun drop même avec l’application fermée</p>
+              <p className="text-center text-xs font-space leading-3">Ne rate aucun drop même avec l’application fermée</p>
             </div>
             <div className="flex items-center justify-center">
 							<Image src="/assets/radar.png" width={100} height={100} alt="Radar" />
@@ -18,15 +18,15 @@ export default function RadarMode() {
           </div>
           <div className="flex flex-col items-center justify-center w-full h-full">
             <div className="flex flex-col items-center justify-center w-full h-1/2 text-justify">
-              <p className="text-sm mb-2 mt-5 text-center">Comment ça marche ?</p>
-              <p className="text-xs leading-3 text-gray-500">Nous utilisons une fonctionnalité de ton téléphone qui permet de suivre ta position en arrière plan, cela nous permet de t’envoyer une notification lorsque nous detectons que tu passe à côté d’un drop !</p>
+              <p className="text-sm mb-2 mt-5 font-space text-center">Comment ça marche ?</p>
+              <p className="text-xs leading-3 font-space text-gray-500">Nous utilisons une fonctionnalité de ton téléphone qui permet de suivre ta position en arrière plan, cela nous permet de t’envoyer une notification lorsque nous detectons que tu passe à côté d’un drop !</p>
             </div>  
             <div className="flex flex-col items-center justify-center w-full h-1/2">
-              <p className="text-darkGray body-font font-space-300 text-sm mb-2 mt-5 text-center">Est-ce que je vais perdre de la batterie ?</p>
-              <p className="text-gray-500 text-xs text-justify leading-3">L’application detecte automatiquement quand tu te deplace, le reste du temps elle reste inactive pour preserver un maximum la batterie. Tu ne devrais pas remarque de changements après avoir activé le mode radar.</p>
+              <p className="text-darkGray body-font font-space text-sm mb-2 mt-5 text-center">Est-ce que je vais perdre de la batterie ?</p>
+              <p className="text-gray-500 font-space text-xs text-justify leading-3">L’application detecte automatiquement quand tu te deplace, le reste du temps elle reste inactive pour preserver un maximum la batterie. Tu ne devrais pas remarque de changements après avoir activé le mode radar.</p>
             </div>  
             <div className="flex flex-col items-center justify-center w-full h-1/2">
-              <p className="text-sm mb-2 mt-5 text-center">Est-ce que vous me suivez à la trace ?</p>
+              <p className="text-sm mb-2 mt-5 text-center font-space">Est-ce que vous me suivez à la trace ?</p>
               <p className="text-xs text-gray-500 text-justify leading-3 font-space">Nous utilisons ta position uniquement pour calculer ta distance avec les drops autour de toi, une fois trouvés, ta position n’est pas sauvegardée. Il est donc impossible de te tracer avec Dropy !</p>
             </div>  
           </div>
