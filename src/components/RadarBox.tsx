867aab0c203703434bc43aac3b97e234896bcd82
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-14 11:22:53 +0100

    style: change tailwind to scss

--- src/components/RadarBox.tsx
+++ src/components/RadarBox.tsx
@@ -19,17 +19,17 @@ const RadarMode: NextPage = () => {
 						</div>
           </div>
           <div className={styles.tutorial_container}>
-            <div className="flex flex-col items-center justify-center w-full h-1/2 text-justify">
-              <p className="text-sm mb-2 mt-5 font-medium font-space text-center text-darkGrey">Comment ça marche ?</p>
-              <p className="text-xs leading-3 font-space text-grey">Nous utilisons une fonctionnalité de ton téléphone qui permet de suivre ta position en arrière plan, cela nous permet de t’envoyer une notification lorsque nous detectons que tu passe à côté d’un drop !</p>
+            <div className={styles.tutorial_inner}>
+              <p className={styles.tutorial_title}>Comment ça marche ?</p>
+              <p className={styles.tutorial_text}>Nous utilisons une fonctionnalité de ton téléphone qui permet de suivre ta position en arrière plan, cela nous permet de t’envoyer une notification lorsque nous detectons que tu passe à côté d’un drop !</p>
             </div>  
-            <div className="flex flex-col items-center justify-center w-full h-1/2">
-              <p className="text-darkGray body-font font-medium font-space text-sm mb-2 mt-5 text-center text-darkGrey">Est-ce que je vais perdre de la batterie ?</p>
-              <p className="text-grey font-space text-xs text-justify leading-3">L’application detecte automatiquement quand tu te deplace, le reste du temps elle reste inactive pour preserver un maximum la batterie. Tu ne devrais pas remarque de changements après avoir activé le mode radar.</p>
+            <div className={styles.tutorial_inner}>
+              <p className={styles.tutorial_title}>Est-ce que je vais perdre de la batterie ?</p>
+              <p className={styles.tutorial_text}>L’application detecte automatiquement quand tu te deplace, le reste du temps elle reste inactive pour preserver un maximum la batterie. Tu ne devrais pas remarque de changements après avoir activé le mode radar.</p>
             </div>  
-            <div className="flex flex-col items-center justify-center w-full h-1/2">
-              <p className="text-sm mb-2 mt-5 text-center font-medium font-space text-darkGrey">Est-ce que vous me suivez à la trace ?</p>
-              <p className="text-xs text-grey text-justify leading-3 font-space">Nous utilisons ta position uniquement pour calculer ta distance avec les drops autour de toi, une fois trouvés, ta position n’est pas sauvegardée. Il est donc impossible de te tracer avec Dropy !</p>
+            <div className={styles.tutorial_inner}>
+              <p className={styles.tutorial_title}>Est-ce que vous me suivez à la trace ?</p>
+              <p className={styles.tutorial_text}>Nous utilisons ta position uniquement pour calculer ta distance avec les drops autour de toi, une fois trouvés, ta position n’est pas sauvegardée. Il est donc impossible de te tracer avec Dropy !</p>
             </div>  
           </div>
         </div>
