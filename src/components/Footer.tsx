ee292dc0278520abf86d6939e11948ba57b2cc6f
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-27 19:36:40 +0100

    feat: add style footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { type NextPage } from 'next';
+import styles from '../assets/styles/Footer.module.scss';
 
 const Footer: NextPage = () => 
-    <div>
+    <div className={styles.footer}>
       <p>Dropy | @2022 | </p>
-      <a href="">contact@dropy-app.com | </a>
-    <a href="">privacy policy | </a>
-    <a href="">terms & conditions | </a>
+      <a href=""> contact@dropy-app.com | </a>
+    <a href=""> privacy policy | </a>
+    <a href=""> terms & conditions | </a>
     </div>
 
 export default Footer;
