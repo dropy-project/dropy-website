72b932b10f487afd5ed8a75040f85f9b8cba64de
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-27 19:24:46 +0100

    feat: add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { type NextPage } from 'next';
+
+const Footer: NextPage = () => 
+    <div>
+      <p>Dropy | @2022 | </p>
+      <a href="">contact@dropy-app.com | </a>
+    <a href="">privacy policy | </a>
+    <a href="">terms & conditions | </a>
+    </div>
+
+export default Footer;
