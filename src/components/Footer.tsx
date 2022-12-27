1d52f38b126866e6528d2ba59d6052cd7665729c
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-27 19:45:02 +0100

    feat: add mailto

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import styles from '../assets/styles/Footer.module.scss';
 const Footer: NextPage = () => 
     <div className={styles.footer}>
       <p>Dropy | @2022 |</p>
-      <a href="">contact@dropy-app.com |</a>
+      <a href="mailto:contact@dropy-app.com">contact@dropy-app.com |</a>
     <a href="">privacy policy |</a>
     <a href="">terms & conditions |</a>
     </div>
