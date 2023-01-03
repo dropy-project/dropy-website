71a40ec2b630aa5033cd02e4097d4fd48ad38ab6
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-03 09:16:14 +0100

    feat: add ResetPassword box component

---
+++ src/components/ResetPassword.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { type NextPage } from 'next';
+import styles from 'assets/styles/ResetPassword.module.scss';
+
+const ResetPassword: NextPage = () => (
+  <div className={styles.reset_password}>
+    <div className={styles.box}>
+        </div>
+    </div>
+);
+
+export default ResetPassword;
