c4ce160055a0fa15fcca7ecad28654bbe0b24c3e
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-11 15:15:12 +0100

    feat: add copyright symbol

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import styles from '../assets/styles/Footer.module.scss';
 
 const Footer: NextPage = () => 
     <div className={styles.footer}>
-      <p>Dropy | @2022 |</p>
+      <p>Dropy | ©2023 |</p>
       <a href="mailto:contact@dropy-app.com">contact@dropy-app.com |</a>
     <a href="/privacy-policy">privacy policy |</a>
     <a href="/terms-conditions">terms & conditions |</a>
