2591bdd9026e99e131bf67b9761eba47bdb93b2d
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-11 15:14:21 +0100

    fix: font

--- src/components/ResetPasswordBox.tsx
+++ src/components/ResetPasswordBox.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { type NextPage } from 'next';
-import styles from 'assets/styles/ResetPassword.module.scss';
+import styles from '../assets/styles/ResetPasswordBox.module.scss';
 
 const ResetPasswordBox: NextPage = () => (
   <div className={styles.reset_password}>
     <div className={styles.box}>
+    <div className={styles.inner_box}>
+        <div>
+          <p className={styles.title}>Modification du mot-de-passe</p>
+          <div className={styles.profile_container}>
+            <p>@Pseudo</p>
+          </div>
         </div>
+        <div className={styles.fields}>
+          <div className={styles.field}>
+            <input type='password' name='password' id='password' placeholder='Nouveau mot de passe' />
+          </div>
+          <div className={styles.field}>
+            <input type='password' name='password' id='password' placeholder='Confirmation du mot de passe' />
+          </div>
+        </div>  
+      </div>
     </div>
+  </div>
 );
 
 export default ResetPasswordBox;
