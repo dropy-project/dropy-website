11fd080a73a0da82b20526af413a1430629d81dc
celian-rib <celian.riboulet@gmail.com>
2023-02-05 16:47:48 +0100

    Fix format

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { type NextPage } from 'next';
 import styles from '../assets/styles/Footer.module.scss';
 
-const Footer: NextPage = () => 
-    <div className={styles.footer}>
-      <p>Dropy | ©2023 |</p>
-      <a href="mailto:contact@dropy-app.com">contact@dropy-app.com |</a>
-    <a href="/privacy-policy">privacy policy |</a>
-    <a href="/terms-conditions">terms & conditions |</a>
-    </div>
+const Footer: NextPage = () => (
+  <div className={styles.footer}>
+    <p>Dropy | ©2023 |</p>
+    <a href='mailto:contact@dropy-app.com'>contact@dropy-app.com |</a>
+    <a href='/privacy-policy'>privacy policy |</a>
+    <a href='/terms-conditions'>terms & conditions</a>
+  </div>
+);
 
 export default Footer;
