3f8e07d4e7cadbd6ce5d8c9252ad562ee81b3e32
Enzo Mourany <enzo.mourany@outlook.com>
2023-02-27 13:58:21 +0100

    feat: add layout

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Dropy Website',
+  description: 'Dropy',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="fr">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
