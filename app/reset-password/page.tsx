3d19d7e99e907228de95c037b0070df09e46097e
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-11 23:16:13 +0100

    feat: add ResetPasswordBox component

---
+++ app/reset-password/page.tsx
@@ -0,0 +1,26 @@
+import styles from '../../styles/ResetPasswordBox.module.scss';
+
+const ResetPasswordBox = () => (
+  <div className={styles.container}>
+    <div className={styles.box}>
+      <div className={styles.inner_box}>
+        <div>
+          <p className={styles.title}>Modification du mot-de-passe</p>
+          <div className={styles.profile_container}>
+            <p>@Pseudo</p>
+          </div>
+        </div>
+        <div className={styles.fields}>
+          <div className={styles.field}>
+            <input type='password' name='password' id='password' placeholder='Nouveau mot de passe' />
+          </div>
+          <div className={styles.field}>
+            <input type='password' name='password' id='password' placeholder='Confirmation du mot de passe' />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default ResetPasswordBox;
