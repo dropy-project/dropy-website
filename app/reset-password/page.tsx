773571757dac8e6024a8b2c683b27095eb1bd11c
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-12 11:57:33 +0100

    feat: add react icons

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,26 +1,39 @@
+'use client';
+
+import { useState } from 'react';
+import { BsEyeFill, BsEyeSlashFill } from 'react-icons/bs';
 import styles from '../../styles/ResetPasswordBox.module.scss';
 
-const ResetPasswordBox = () => (
-  <div className={styles.container}>
-    <div className={styles.box}>
-      <div className={styles.inner_box}>
-        <div className={styles.header}>
-          <p className={styles.title}>Modification du mot-de-passe</p>
-          <div className={styles.profile_container}>
-            <p>@Pseudo</p>
+const ResetPasswordBox = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <div className={styles.inner_box}>
+          <div className={styles.header}>
+            <p className={styles.title}>Modification du mot-de-passe</p>
+            <div className={styles.profile_container}>
+              <p>@Pseudo</p>
+            </div>
           </div>
-        </div>
-        <div className={styles.fields}>
-          <div className={styles.field}>
-            <input type='password' name='password' id='password' placeholder='Nouveau mot de passe' />
-          </div>
-          <div className={styles.field}>
-            <input type='password' name='password' id='password' placeholder='Confirmation du mot de passe' />
+          <div className={styles.fields}>
+            <div className={styles.field}>
+              <input type={showPassword ? 'text' : 'password'}name='password' id='password' placeholder='Nouveau mot de passe' />
+              {showPassword ? <BsEyeSlashFill onClick={toggleShowPassword} /> : <BsEyeFill onClick={toggleShowPassword} />}
+            </div>
+            <div className={styles.field}>
+              <input type='password' name='password' id='password' placeholder='Confirmation du mot de passe' />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ResetPasswordBox;
