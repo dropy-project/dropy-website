52cecb52cc237b7cc53b310d128cac3acdc686ca
Enzo Mourany <enzo.mourany@outlook.com>
2023-03-13 11:02:38 +0100

    ci: finish Button logic

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,15 +1,15 @@
+'use client';
+
 import React from 'react';
 
 import styles from '../styles/Button.module.scss';
 
 interface ButtonProps {
-  action: () => void;
   text: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({ action, text }) => (
+export const Button: React.FC<ButtonProps> = ({ text }) => (
   <button
-    onClick={action}
     type='button'
     className={styles.button}
   >
